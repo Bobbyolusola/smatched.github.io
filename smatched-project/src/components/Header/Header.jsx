886f8937a0b7c6aca9c08eddc3773f1fb9a783a8
olusolabobby <olusolaadelabu89@gmail.com>
2023-09-05 15:11:42 +0300

    header 2

--- smatched-project/src/components/Header/Header.jsx
+++ smatched-project/src/components/Header/Header.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
+
+    const [isActive, setIsActive] = useState(false);
 
     return(
         <>
             <div className={styles.mainBox}>
                 <div className={styles.headerBox}>
                     <div className={styles.right}>
+
                         <div className={styles.logo}>
                             <img src="/img/Horizontal container.png" alt=''/>
                         </div>
-                        <div className={styles.menu}>
-                            <span>Product</span>
-                            <span>Our Mission</span>
-                            <span>Influencer</span>
-                            <span>For Business</span>
-                            <span>Blog</span>
+
+                        <div className={styles.topMenu}>
+                            <ul>Product</ul>
+                            <ul>Our Mission</ul>
+                            <ul>Influencer</ul>
+
+                            <ul className={styles.dropDown}>
+                                <ul className={styles.dropDownBtn}
+                                onClick = {(e) =>
+                                    setIsActive (!isActive)
+                                }>
+                                    For Business <img className={styles.arrowDown} src='/img/arrow-down-s-line (1).svg' alt='' />
+                                </ul>
+                                {isActive && (
+                                    <ul className={styles.dropDownContent}>
+                                        <li className={styles.dropDownItem}>Game Industry</li>
+                                        <li className={styles.dropDownItem}>Nonprofit Companies</li>
+                                        <li className={styles.dropDownItem}>Newspaper Publishing Industry</li>
+                                        <li className={styles.dropDownItem}>Mobile App Monetization</li>
+                                        <li className={styles.dropDownItem}>Website Monetization</li>
+                                    </ul>
+                                )}
+                            </ul>
+
+                            <ul>Blog</ul>
                         </div>
                     </div>
                     <div className={styles.left}>
@@ -29,3 +51,5 @@ const Header = () => {
 }
 
 export default Header;
+
+
